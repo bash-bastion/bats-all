edf9bba35f32c78f1d285b9c305efba0e91d1f55
Edwin Kofler <edwin@kofler.dev>
2021-10-02 22:12:52 -0700

    fix: test_helper.bash properly load dependencies

--- bats-file-test/fixtures/temp/test_helper.bash
+++ bats-file-test/fixtures/temp/test_helper.bash
@@ -1,8 +1,5 @@
 export TEST_MAIN_DIR="${BATS_TEST_DIRNAME}/../../.."
 export TEST_DEPS_DIR="${TEST_DEPS_DIR-${TEST_MAIN_DIR}/..}"
 
-# Load dependencies.
-load "${TEST_DEPS_DIR}/bats-support/load.bash"
-
-# Load library.
+# Load dependencies and library.
 load "${TEST_MAIN_DIR}/load.bash"
