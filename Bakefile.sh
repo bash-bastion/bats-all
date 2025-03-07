4f26fc34025ddee1875fc9f6667826c5afdd5bd8
Edwin Kofler <edwin@kofler.dev>
2025-03-07 01:32:55 -0800

    Add publish instructions to Bakefile

--- Bakefile.sh
+++ Bakefile.sh
@@ -14,3 +14,10 @@ task.update() {
 		git subtree --squash -P "$repo" pull "https://github.com/bats-core/$repo" HEAD
 	done
 }
+
+task.publish() {
+   # Update version in `basalt.toml`
+   # Update version in `package.json`
+   # Append new changes to `CHANGELOG.md`
+   npm publish
+}
